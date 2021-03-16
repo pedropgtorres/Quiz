e7df2a81ec4a696819f2ef14043ee81fa8311662
Paulo Torres <34400704+pedropgtorres@users.noreply.github.com>
2021-03-16 10:28:18 -0300

    evolução
    evoluindo app

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -3,14 +3,47 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget{
+
+  void Resposta(){
+    print('Resposta 1...');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quiz"),
         ),
-        body: Text("Hello!!!"),
+        body: Column(
+          children: [
+            Text(perguntas.elementAt(0)),
+            ElevatedButton(
+                onPressed: Resposta,
+                child: Text('Resposta 1')
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  print('Resposta 2...');
+                },
+                child: Text('Resposta 2')
+            ),
+            ElevatedButton(
+                onPressed: () => print('Resposta 3...'),
+                child: Text('Resposta 3')
+            ),
+            ElevatedButton(
+                onPressed: null,
+                child: Text('Apenas para exibir um botão desabilitado')
+            )
+          ],
+        ),
       ),
     );
   }
